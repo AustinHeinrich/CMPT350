538b7b911f2e05c2eeae3c7a503798c5c0afa07e
AustinHeinrich <austinwheinrich@gmail.com>
2019-02-14 01:00:35 -0600

    built frontend for part B to a 'satisfiable'/'similar to example' degree

--- a2/functions.js
+++ a2/functions.js
@@ -1,15 +1,36 @@
 function mkBoard() {
     var aBoard;
     aBoard = document.getElementById("newboard").value;
+    document.getElementById("newboard").value = "";  // clear text
 
-    document.getElementById("newboard").value = "";
+    var boardList = document.getElementById("boardlist");
+    var useCheck = false; // True if aBoard has been used previously
+    for (var i = 0; i < boardList.length; i++) {
+        if (boardList.options[i].value == aBoard) {
+            alert("That is already a board.");
+            useCheck = true;
+            break;
+        }
+    }
 
-    addBoard(aBoard);
+    if (useCheck == false) addBoard(aBoard);
 }
 
 function addBoard(aBoard) {
-    boardData.push(aBoard)
-    document.getElementById("boardlist").
+
+    var boardList = document.getElementById("boardlist");
+    boardList.options.length = 0;
+
+    boardData.push(aBoard);
+    boardData.sort();
+
+    for (var i = 0; i < boardData.length; i++) {
+        var opt = boardData[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        boardList.appendChild(el);
+    }
 }
 
 var boardData = new Array();
--- a2/server.js
+++ a2/server.js
@@ -5,4 +5,4 @@ const port = 3000;
 app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile(__dirname + '/messageboard.html'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
