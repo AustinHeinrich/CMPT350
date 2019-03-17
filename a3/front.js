2932a8a7a16db70535d3855461e01d6261e3a3b9
AustinHeinrich <austinwheinrich@gmail.com>
2019-03-17 11:26:36 -0600

    Commiting files I forgot to yesterday.

---
+++ a3/front.js
@@ -0,0 +1,30 @@
+// set up authorization -- very secure
+const axiosSettings = {
+  auth: {
+    username: "admin",
+    password: "password"
+  }
+};
+
+async function getChatRooms() {
+  try {
+    const res = await axios.get("/messageboards", axiosSettings);
+    const chatrooms = res.data;
+
+    //console.log(chatrooms);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function createNewBoard() {}
+
+async function deleteBoard() {}
+
+async function postMessage() {}
+
+async function main() {
+  getChatRooms();
+}
+
+main();
