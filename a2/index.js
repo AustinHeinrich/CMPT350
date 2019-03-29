4bb096ac62134dea877e2eb752cbb60efb4601e1
AustinHeinrich <austinwheinrich@gmail.com>
2019-03-28 21:03:21 -0600

    Changed tab spacing in these by accident (but I like this better)

--- a2/index.js
+++ a2/index.js
@@ -4,7 +4,7 @@
  * 11177796
  */
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
@@ -14,59 +14,59 @@ app.use(express.static(__dirname));
 app.use(express.json());
 
 /* GET home */
-app.get('/', (req, res) => res.sendFile(__dirname + '/messageboards.html'));
+app.get("/", (req, res) => res.sendFile(__dirname + "/messageboards.html"));
 app.listen(port, () => console.log(`App listening on port ${port}.`));
 
-app.get('/messageboards', (req, res) => {
-    res.status(200).send(Object.keys(messageboards));
+app.get("/messageboards", (req, res) => {
+  res.status(200).send(Object.keys(messageboards));
 });
 
-app.get('/messages/:msgBoard', (req, res) => {
-    // checks that the board exists
-    if (!(req.params.msgBoard in messageboards)) {
-        res.status(400).send({
-            message: "Board does not exist."
-        });
-        
-        return;
-    }
+app.get("/messages/:msgBoard", (req, res) => {
+  // checks that the board exists
+  if (!(req.params.msgBoard in messageboards)) {
+    res.status(400).send({
+      message: "Board does not exist."
+    });
 
-    res.status(200).send(messageboards[req.params.msgBoard]);
+    return;
+  }
+
+  res.status(200).send(messageboards[req.params.msgBoard]);
 });
 
-app.post('/messageboards', (req, res) => {
-    var messageBoard = req.body;    // e.g. "{ boards: 'hello' }"
-    var board = messageBoard.board; // e.g. "hello"
+app.post("/messageboards", (req, res) => {
+  var messageBoard = req.body; // e.g. "{ boards: 'hello' }"
+  var board = messageBoard.board; // e.g. "hello"
 
-    messageboards[board] = [];
-    res.status(200).send({message : "ok"});
+  messageboards[board] = [];
+  res.status(200).send({ message: "ok" });
 });
 
-app.post('/messages/:msgBoard', (req, res) => {
-    if (!(req.params.msgBoard in messageboards)) {
-        res.status(400).send({
-            message : "Board does not exist."
-        });
-        
-        return;
-    }
+app.post("/messages/:msgBoard", (req, res) => {
+  if (!(req.params.msgBoard in messageboards)) {
+    res.status(400).send({
+      message: "Board does not exist."
+    });
 
-    if (!("message" in req.body)) {
-        res.status(400).send({
-            message : "There is no message."
-        });
+    return;
+  }
 
-        return;
-    }
+  if (!("message" in req.body)) {
+    res.status(400).send({
+      message: "There is no message."
+    });
 
-    if (req.body.message.length == 0) {
-        res.status(400).send({
-            message : "Message must have content."
-        });
+    return;
+  }
 
-        return;
-    }
+  if (req.body.message.length == 0) {
+    res.status(400).send({
+      message: "Message must have content."
+    });
 
-    messageboards[req.params.msgBoard].push(req.body.message);
-    res.status(200).send({message : "ok"});
+    return;
+  }
+
+  messageboards[req.params.msgBoard].push(req.body.message);
+  res.status(200).send({ message: "ok" });
 });
